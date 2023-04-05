4e15d6f0e2135317d1473ac3a5255c1b0750045e
Bertoldijulian <julianbertoldi12@hotmail.com.ar>
2023-04-05 09:51:51 -0300

    creacion archivo para ejercicios

---
+++ ejercicios.js
@@ -0,0 +1,8 @@
+
+const datosPropios = Array ("julian", 26, false, 22/02/1997)
+
+const libroFavorito = {
+    titulo: "A Song of Ice and Fire",
+    autor: "George R. R. Martin",
+    fecha: 08/1996,
+}
