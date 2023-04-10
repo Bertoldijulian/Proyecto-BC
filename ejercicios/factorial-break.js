f1333c5cbc1eaba173173230bec157f4bc31ec2f
Bertoldijulian <julianbertoldi12@hotmail.com.ar>
2023-04-10 14:53:06 -0300

    for, while, break

---
+++ ejercicios/factorial-break.js
@@ -0,0 +1,9 @@
+let x = 10;
+let i = x-1;
+
+while(x){
+    x *= i;
+    i--;
+    if (i <= 1) break;
+}
+console.log(x);
---
+++ ejercicios/factorial-for.js
@@ -0,0 +1,6 @@
+total = 1
+
+for (i = 1; i < 10; i++) {
+    total = total * i
+    console.log(total)
+}
