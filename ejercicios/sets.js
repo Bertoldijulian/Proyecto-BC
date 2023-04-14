78dfcd813b158e58fcc89e6173d2f3b06c76d735
Bertoldijulian <julianbertoldi12@hotmail.com.ar>
2023-04-14 09:00:08 -0300

    sets

---
+++ ejercicios/sets.js
@@ -0,0 +1,9 @@
+//Un nuevo Set con los nombres de tu familia
+const array = ['Julian', 'Victoria', 'Rocio', 'Carlos', 'Laura']
+const familia = new Set(array)
+
+//Modifica el Set original añadiendo tu nombre (duplicado) (debería darte lo mismo)
+familia.add('Julian')
+
+//Modifica el Set original añadiendo el nombre "Javascript" (ya que empieza a formar parte de tu vida
+familia.add('Javascript')
